801cf23ed5155b12ffa7d11fcbb7f2ddd96c50a5
Charles Changmin Han <name.cmh@gmail.com>
2015-06-21 21:42:24 -0700

    Validate post contents on submission.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,3 +1,16 @@
+Template.postSubmit.onCreated(function() {
+  Session.set('postSubmitErrors', {});
+});
+
+Template.postSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function(field) {
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
+  }
+});
+
 Template.postSubmit.events({
   'submit form': function(e) {
     e.preventDefault();
@@ -7,8 +20,11 @@ Template.postSubmit.events({
       title: $(e.target).find('[name=title]').val()
     };
 
-    //post._id = Posts.insert(post);
-    //Router.go('postPage', post);
+    var errors = validatePost(post);
+    if (errors.title || errors.url) {
+      return Session.set('postSubmitErrors', errors);
+    }
+
 
     Meteor.call('postInsert', post, function(error, result) {
       if (error) {
